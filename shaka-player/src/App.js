ed4ebb0bf3854aa28ccf99880871c3c34edbe3fb
kumarreddyyarraballi <yarra_kumar@discovery.com>
2022-07-08 18:17:56 +0530

    Updates

--- shaka-player/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ShakaPlayer from 'shaka-player-react';
-import 'shaka-player/dist/controls.css';
-
-function App() {
-  return <ShakaPlayer  autoPlay={true} src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" />;
-}
-
-export default App;
